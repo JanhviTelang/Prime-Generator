a31fdf29825c33df5daddbb70297a9005efe9f47
JanhviTelang <63359344+JanhviTelang@users.noreply.github.com>
2020-05-02 19:32:47 +0530

    Create primegenerator.py

---
+++ primegenerator.py
@@ -0,0 +1,15 @@
+list=[]
+t=int(input("Enter the number of cases:"))
+for l in range(0,t):
+    a=eval(input("Enter the lower and upper limit:"))
+    list.append(a)
+for i in range(0,t):
+    m=list[i][0]
+    n=list[i][1]
+    for j in range(m,n+1):
+        h=0
+        for k in range(1,j+1):
+            if j%k==0:
+                h=h+1
+        if h==2:
+            print(j)
